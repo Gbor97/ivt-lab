f49895b5515b1c0973f6e789d185ab4491731a23
Ramacs Gabor <ramacsgabor9@gmail.com>
2019-04-12 13:20:09 +0200

    fireTorpedo implemented

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -16,8 +16,8 @@ public class GT4500 implements SpaceShip {
   }
 
   public boolean fireLaser(FiringMode firingMode) {
-    // TODO not implemented yet
-    return false;
+
+        return false;
   }
 
   /**
@@ -79,8 +79,8 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
-
-        break;
+		return true;
+        
     }
 
     return firingSuccess;
